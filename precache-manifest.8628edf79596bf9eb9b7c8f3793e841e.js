9ca93b69ac24146f56237c327768733072c5ed5c
Rajkumar Balusamy <rajkumar.balusamy1@tesco.com>
2020-06-28 11:07:56 +0530

    Updates

---
+++ precache-manifest.8628edf79596bf9eb9b7c8f3793e841e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95e64e756d7b47a0bba39735274da203",
+    "url": "./index.html"
+  },
+  {
+    "revision": "41a25269021af51a583e",
+    "url": "./static/css/main.0e1794ba.chunk.css"
+  },
+  {
+    "revision": "ccf34c38eb57894c1095",
+    "url": "./static/js/2.9f6319d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.9f6319d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41a25269021af51a583e",
+    "url": "./static/js/main.4ab6b332.chunk.js"
+  },
+  {
+    "revision": "3e5f531a884c5255952e",
+    "url": "./static/js/runtime-main.f60f16d6.js"
+  }
+]);
